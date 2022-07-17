daf77342c3254ce53b701b427313ec7dd34f3033
harshitsingh0302 <81532133+harshitsingh0302@users.noreply.github.com>
2022-07-18 00:08:42 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,11 @@
+dict = {"name": "",
+        "cat": ""}
+
+text = [input()]
+
+def convert(text):
+    return (text[0].split())
+
+print(convert(text))
+
+print(dict["name"] + dict["cat"])
