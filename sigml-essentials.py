1dd371f07e6245b7a34d35629a070595a9fc4fbd
osBins <arpitjain.2001@gmail.com>
2022-12-04 16:29:32 +0530

    dict

--- sigml-essentials.py
+++
@@ -1,2 +0,0 @@
-tags = {"start": """<?xml version="1.0" encoding="UTF-8"?>""",
-        "end": "</sigml>"}
--- test.py
+++
@@ -1,9 +0,0 @@
-def sliceSigml():
-    with open('SiGML-output.sigml', 'r') as file:
-        data = file.read().split("\n")
-        data = data[1:-1]
-
-    with open('lol.sigml', 'w') as file:
-        for line in data:
-            file.write(line)
-            file.write("\n")
