bb3d91067bdeb0cc04f333500577e3b97ade6553
osBins <arpitjain.2001@gmail.com>
2023-05-08 14:40:08 +0530

    pressing button

--- st.py
+++ st.py
@@ -9,6 +9,6 @@ video_bytes = video_file.read()
 st.video(video_bytes)
 
 webPlayerHTMLFile = open('./cwasa.html', 'r')
-str = webPlayerHTMLFile.read()
+cwasaHTML = webPlayerHTMLFile.read()
 
-st.components.v1.html(str, width=None, height=500)
+st.components.v1.html(cwasaHTML, width=None, height=500)
